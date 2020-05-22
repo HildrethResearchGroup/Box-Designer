a24401a52a91916189d8ea63116f67e5a571e2af
Owen Hildreth <ohildreth@mines.edu>
2020-05-22 11:25:05 -0600

    Initial Commit

---
+++ Box DesignerTests/Box_DesignerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Box_DesignerTests.swift
+//  Box DesignerTests
+//
+//  Created by Owen Hildreth on 5/22/20.
+//  Copyright © 2020 Hildreth Research Group. All rights reserved.
+//
+
+import XCTest
+@testable import Box_Designer
+
+class Box_DesignerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

4c8c1378b5bc87b6f8b94f24b1451cdd61381384
Box_Designer_group <cgougenheim.third@gmail.com>
2020-06-01 15:27:38 -0500

    three walls for our box complete..ish

--- Box Designer/MainViewController.swift
+++ Box Designer/MainViewController.swift
@@ -32,29 +32,59 @@ class MainViewController: NSViewController {
     }
     
     //Charles Gougenheim
-    func createBox() -> SCNShape {
+    func createBox(length: Double, width: Double, height: Double) -> [SCNShape] {
         // create bezier path
-        let path = NSBezierPath()
-        path.move(to: CGPoint(x: 0.0, y: 0.0)) // point A
-        path.line(to: CGPoint(x: 0.0, y: 0.5)) // point B
-        path.line(to: CGPoint(x: 0.5, y: 0.5)) // point C
-        path.line(to: CGPoint(x: 0.5, y: 0.0)) // point D
-        path.close()
+        var boxWalls = [SCNShape]()
+        let path1 = NSBezierPath()
+        path1.move(to: CGPoint(x: 0.0, y: 0.0)) // point A
+        path1.line(to: CGPoint(x: 0.0, y: height)) // point B
+        path1.line(to: CGPoint(x: length, y: height)) // point C
+        path1.line(to: CGPoint(x: length, y: 0.0)) // point D
+        path1.close()
         
-        //return path
+        let path2 = NSBezierPath()
+        path2.move(to: CGPoint(x: 0.0, y: 0.0)) // point A
+        path2.line(to: CGPoint(x: 0.0, y: height)) // point B
+        path2.line(to: CGPoint(x: length, y: height)) // point C
+        path2.line(to: CGPoint(x: length, y: 0.0)) // point D
+        path2.close()
+        
+        let path3 = NSBezierPath()
+        path3.move(to: CGPoint(x: 0.0, y: 0.0)) // point A
+        path3.line(to: CGPoint(x: 0.0, y: height)) // point B
+        path3.line(to: CGPoint(x: length, y: height)) // point C
+        path3.line(to: CGPoint(x: length, y: 0.0)) // point D
+        path3.close()
 
         // create a geometry : SCNShape
-        let shape = SCNShape(path: path, extrusionDepth: 0.2)
-        let color = #colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1)
-        shape.firstMaterial?.diffuse.contents = color
+        let shape1 = SCNShape(path: path1, extrusionDepth: 0.2)
+        let shape2 = SCNShape(path: path2, extrusionDepth: 0.2)
+        let shape3 = SCNShape(path: path3, extrusionDepth: 0.2)
+//        let shape4 = SCNShape(path: path4, extrusionDepth: 0.2)
+//        let shape5 = SCNShape(path: path5, extrusionDepth: 0.2)
+        let color1 = #colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1)
+        let color2 = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        let color3 = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        shape1.firstMaterial?.diffuse.contents = color1
+        shape2.firstMaterial?.diffuse.contents = color2
+        shape3.firstMaterial?.diffuse.contents = color3
+     
+        boxWalls.append(shape1)
+        boxWalls.append(shape2)
+        boxWalls.append(shape3)
+//        boxWalls.append(shape4)
+//        boxWalls.append(shape5)
+
         
-        return shape
+        
+        //boxWalls.append(shape)
+        return boxWalls
         
         // create a node
-        let boxNode = SCNNode(geometry: shape)
+        //let boxNode = SCNNode(geometry: shape)
         
         // position node and add to the scene
-        boxNode.position.z = -1
+        //boxNode.position.z = -1
         
         //sceneView.scene.rootNode.addChildNode(boxNode)
         
@@ -62,12 +92,27 @@ class MainViewController: NSViewController {
     
     // MARK: Scene
     func sceneSetup() {
+        //var boxShape = [SCNShape]()
         let scene = SCNScene()
         /*let boxGeometry = SCNBox(width:4.0, height: 4.0, length: 4.0, chamferRadius: 0.01)
         let boxNode = SCNNode(geometry: boxGeometry)*/
-        let boxShape = createBox()
-        let boxNode = SCNNode(geometry: boxShape)
-        scene.rootNode.addChildNode(boxNode)
+        var length = 1.0
+        var width = 1.0
+        var height = 1.0
+        let boxShape = createBox(length: length, width: width, height: height)
+        
+        //let boxNode = SCNNode(geometry: boxShape)
+        let boxNode1 = SCNNode(geometry: boxShape[0])
+        let boxNode2 = SCNNode(geometry: boxShape[1])
+        let boxNode3 = SCNNode(geometry: boxShape[2])
+
+        boxNode1.position = SCNVector3Make(0, 0, 0)
+        boxNode2.rotation = SCNVector4(0, 1, 0, -1 * CGFloat.pi/2)
+        boxNode3.position = SCNVector3Make(0, 0, 1)
+
+        scene.rootNode.addChildNode(boxNode1)
+        scene.rootNode.addChildNode(boxNode2)
+        scene.rootNode.addChildNode(boxNode3)
         boxView.scene = scene
         lightingSetup(scene)
     }

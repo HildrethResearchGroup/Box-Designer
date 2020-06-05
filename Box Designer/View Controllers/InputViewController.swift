4778fc363cda3a6cac5c9a1074ec581ce78939f5
Box_Designer_group <cgougenheim.third@gmail.com>
2020-06-05 09:33:22 -0500

    Dimension buttons, and labels. Formatted to fit

--- Box Designer/View Controllers/InputViewController.swift
+++ Box Designer/View Controllers/InputViewController.swift
@@ -12,9 +12,21 @@ import SceneKit
 
 class InputViewController: NSViewController {
     
+    //height, width, length text fields for input
     @IBOutlet weak var heightTextField: NSTextField!
     @IBOutlet weak var widthTextField: NSTextField!
     @IBOutlet weak var lengthTextField: NSTextField!
+    
+    // height, width,length buttons
+    @IBOutlet weak var okButtonHeight: NSButton!
+    @IBOutlet weak var okButtonWidth: NSButton!
+    @IBOutlet weak var okButtonLength: NSButton!
+    
+    
+    // height, width,length labels
+    @IBOutlet weak var heightLabel: NSTextField!
+    @IBOutlet weak var widthLabel: NSTextField!
+    @IBOutlet weak var lengthLabel: NSTextField!
     
     @IBOutlet weak var tabWidthSlider: NSSlider!
     

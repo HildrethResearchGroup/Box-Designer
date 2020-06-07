a6bc69edae2b2cf59c4293309affccb19533fd3d
Grace Clark <geclark@mymail.mines.edu>
2020-06-07 00:19:58 -0500

    buttons, textfields, and sliders are all connected with box model.  Need to implement awake from nib and path generator function; path generator function needs to be called by each wall when changes are made to the box model.

--- Box Designer/Model/BoxModel.swift
+++ Box Designer/Model/BoxModel.swift
@@ -17,8 +17,15 @@ class BoxModel {
     var walls: [WallModel]
     
     //This initializer can be used to create a box using data loaded from a file
-    init(_ walls: [WallModel]) {
+    init(_ walls: [WallModel], _ width: Double, _ length: Double, _ height: Double, _ materialThickness: Double, _ innerDimensions: Bool, _ joinType: JoinType, _ tabWidth: Double?) {
         self.walls = walls
+        self.boxWidth = width
+        self.boxLength = length
+        self.boxHeight = height
+        self.materialThickness = materialThickness
+        self.innerDimensions = innerDimensions
+        self.joinType = joinType
+        self.tabWidth = tabWidth
     }
     
     //This initializer creates the default box model which is loaded whenever the application is launched
@@ -32,7 +39,14 @@ class BoxModel {
         let wall3 = WallModel(4.0, 4.0, 0.50, WallType.smallCorner, JoinType.overlap, SCNVector3Make(0.0, 0.0, 0.0), tabWidth: nil)
         let wall4 = WallModel(4.0, 4.0, 0.50, WallType.smallCorner, JoinType.overlap, SCNVector3Make(0.0, 0.0, 4.0), tabWidth: nil)
         var walls = [wall0, wall1, wall2, wall3, wall4]
+        
         self.walls = walls
+        self.boxWidth = 4.0
+        self.boxLength = 4.0
+        self.boxHeight = 4.0
+        self.materialThickness = 0.50
+        self.innerDimensions = false
+        self.joinType = JoinType.overlap
     }
     
     /*
@@ -40,10 +54,22 @@ class BoxModel {
      However, they make the process of adjusting the individual
      walls of the path significantly easier.
      */
-    var boxWidth: Double?
-    var boxLength: Double?
-    var boxHeight: Double?
-    var innerDimensions: Bool?
-    var joinType: JoinType?
+    var boxWidth: Double
+    var boxLength: Double
+    var boxHeight: Double
+    var materialThickness: Double
+    var innerDimensions: Bool
+    var joinType: JoinType
     var tabWidth: Double?
+    
+    func smallestDimension() -> Double {
+        var smallest = boxWidth
+        if boxLength < smallest {
+            smallest = boxLength
+        }
+        if boxHeight < smallest {
+            smallest = boxHeight
+        }
+        return smallest
+    }
 }
--- Box Designer/View Controllers/InputViewController.swift
+++ Box Designer/View Controllers/InputViewController.swift
@@ -9,12 +9,53 @@
 import Foundation
 import Cocoa
 
-class InputViewController: NSViewController {
+class InputViewController: NSViewController, NSTextDelegate {
     
     var boxModel = BoxModel()
     
+    @IBOutlet weak var lengthTextField: NSTextField!
+    @IBOutlet weak var widthTextField: NSTextField!
+    @IBOutlet weak var heightTextField: NSTextField!
+    @IBOutlet weak var materialThicknessTextField: NSTextField!
+    
     @IBOutlet weak var innerOrOuterDimensionControl: NSSegmentedCell!
     @IBOutlet weak var joinTypeControl: NSSegmentedControl!
+    
+    @IBOutlet weak var tabWidthLabel: NSTextField!
+    @IBOutlet weak var tabWidthSlider: NSSlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lengthTextField.doubleValue = boxModel.boxLength
+        widthTextField.doubleValue = boxModel.boxWidth
+        heightTextField.doubleValue = boxModel.boxHeight
+        materialThicknessTextField.doubleValue = boxModel.materialThickness
+        
+        innerOrOuterDimensionControl.selectSegment(withTag: 0)
+        joinTypeControl.selectSegment(withTag: 0)
+        
+        tabWidthSlider.isEnabled = false
+    }
+    
+    @IBAction func lengthTextFieldDidChange(_ sender: Any) {
+        boxModel.boxLength = lengthTextField.doubleValue
+        setSliderLimits()
+    }
+    
+    @IBAction func widthTextFieldDidChange(_ sender: Any) {
+        boxModel.boxWidth = widthTextField.doubleValue
+        setSliderLimits()
+    }
+    
+    @IBAction func heightTextFieldDidChange(_ sender: Any) {
+        boxModel.boxHeight = heightTextField.doubleValue
+        setSliderLimits()
+    }
+    
+    @IBAction func materialThicknessTextFieldDidChange(_ sender: Any) {
+        boxModel.materialThickness = materialThicknessTextField.doubleValue
+    }
     
     @IBAction func innerOrOuterDimensionsSelected(_ sender: Any) {
         let choice = innerOrOuterDimensionControl.selectedSegment
@@ -29,9 +70,22 @@ class InputViewController: NSViewController {
         let choice = joinTypeControl.selectedSegment
         if choice == 0 {
             boxModel.joinType = JoinType.overlap
+            tabWidthSlider.isEnabled = false
         } else if choice == 1 {
             boxModel.joinType = JoinType.tab
+            setSliderLimits()
+            tabWidthSlider.isEnabled = true
         }
     }
     
+    @IBAction func tabWidthChanged(_ sender: Any) {
+        let tabWidth = tabWidthSlider.doubleValue
+        tabWidthLabel.stringValue = String(format: "Tab Width [%.2f]", tabWidth)
+        boxModel.tabWidth = tabWidth
+    }
+    
+    func setSliderLimits() {
+        let smallestDimension = boxModel.smallestDimension()
+        tabWidthSlider.maxValue = smallestDimension/3
+    }
 }

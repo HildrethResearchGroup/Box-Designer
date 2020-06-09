9d496c2bef06c5012f8828c49a4e4cc7b338d3e1
Grace Clark <geclark@mymail.mines.edu>
2020-06-09 13:25:36 -0500

    skeleton of FileHandlingViewController

---
+++ Box Designer/View Controllers/FileHandlingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FileHandlingViewController.swift
+//  Box Designer
+//
+//  Created by Grace Clark on 6/9/20.
+//  Copyright © 2020 Hildreth Research Group. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class FileHandlingViewController: NSViewController {
+    
+    
+    
+}
